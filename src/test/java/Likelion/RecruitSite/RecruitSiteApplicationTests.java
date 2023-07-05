c3436fcb49853b1ff90aabb9f2a7f33b7efc9dbe
HyeonJeong <67951802+12hyeon@users.noreply.github.com>
2023-07-05 23:29:22 +0900

    init

---
+++ src/test/java/Likelion/RecruitSite/RecruitSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package Likelion.RecruitSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
